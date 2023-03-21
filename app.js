7166f74773a8242bfd3abf44e226d96d0a539744
Aman Ahmed <aman16.aa17@gmail.com>
2023-03-21 16:02:34 +0100

    Function Return Types & "void"

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if ((typeof input1 === "number" && typeof input2 === "number") ||
-        resultConversion === "as-number") {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    // if (resultConversion === "as-number") {
-    //   return +result;
-    // } else {
-    //   return result.toString();
-    // }
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Aman";
+if (typeof userInput === "string") {
+    userName = userInput;
 }
-var combinedAges = combine(22, 26, "as-number");
-console.log(combinedAges);
-var combinedStringAges = combine("22", "26", "as-number");
-console.log(combinedStringAges);
-var combinedNames = combine("Aman", "Anna", "as-text");
-console.log(combinedNames);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+    // while (true) {}
+}
+generateError("An error occurred!", 500);
