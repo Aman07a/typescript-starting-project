4cfb37e9c89bf8a886d92e0f92895f66f7755159
Aman Ahmed <aman16.aa17@gmail.com>
2023-03-21 19:53:50 +0100

    Rest Parameters

--- src/app.ts
+++ src/app.ts
@@ -16,18 +16,18 @@ age = 23;
 
 // console.log(isOld);
 
-const add = (a: number, b: number = 1) => a + b;
+// const add = (a: number, b: number = 1) => a + b;
 
-const printOutput: (output: string | number) => void = (output) =>
-  console.log(output);
+// const printOutput: (output: string | number) => void = (output) =>
+//   console.log(output);
 
-const button = document.querySelector("button");
+// const button = document.querySelector("button");
 
-if (button) {
-  button.addEventListener("click", (event) => console.log(event));
-}
+// if (button) {
+//   button.addEventListener("click", (event) => console.log(event));
+// }
 
-printOutput(add(5));
+// printOutput(add(5));
 
 const hobbies = ["Coding", "Gym"];
 const activeHobbies = ["Hiking"];
@@ -40,3 +40,12 @@ const person = {
 };
 
 const copiedPerson = { ...person };
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
