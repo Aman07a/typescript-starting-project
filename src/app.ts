08b764dc574e75f74a7ed6a4b3fcd00a5d5eba60
Aman Ahmed <aman16.aa17@gmail.com>
2023-03-21 20:22:48 +0100

    Creating a First Class

--- src/app.ts
+++ src/app.ts
@@ -1,59 +1,11 @@
-// const userName = "Aman";
-// // userName = "Aman";
-// let age = 22;
+class Department {
+  name: string;
 
-// age = 23;
+  constructor(n: string) {
+    this.name = n;
+  }
+}
 
-// function add(a: number, b: number) {
-//   let result;
-//   result = a + b;
-//   return result;
-// }
+const accounting = new Department("Accounting");
 
-// if (age > 20) {
-//   let isOld = true;
-// }
-
-// console.log(isOld);
-
-// const add = (a: number, b: number = 1) => a + b;
-
-// const printOutput: (output: string | number) => void = (output) =>
-//   console.log(output);
-
-// const button = document.querySelector("button");
-
-// if (button) {
-//   button.addEventListener("click", (event) => console.log(event));
-// }
-
-// printOutput(add(5));
-
-const hobbies = ["Coding", "Gym"];
-const activeHobbies = ["Hiking"];
-
-activeHobbies.push(...hobbies);
-
-const person = {
-  firstName: "Aman",
-  age: 22,
-};
-
-const copiedPerson = { ...person };
-
-const add = (...numbers: number[]) => {
-  return numbers.reduce((curResult, curValue) => {
-    return curResult + curValue;
-  }, 0);
-};
-
-const addedNumbers = add(5, 10, 2, 3.7);
-console.log(addedNumbers);
-
-const [hobby1, hobby2, ...remainingHobbies] = hobbies;
-
-console.log(hobbies, hobby1, hobby2);
-
-const { firstName: userName, age } = person;
-
-console.log(userName, age, person);
+console.log(accounting);
