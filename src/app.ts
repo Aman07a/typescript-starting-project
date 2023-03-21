69bd906f8428a9b0c6510d364026cd3bcfaa293a
Aman Ahmed <aman16.aa17@gmail.com>
2023-03-21 19:07:16 +0100

    "let" and "const"

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-// Code goes here!
+const userName = "Aman";
+// userName = "Aman";
+let age = 22;
+
+age = 23;
+
+function add(a: number, b: number) {
+  let result;
+  result = a + b;
+  return result;
+}
+
+// if (age > 20) {
+//   let isOld = true;
+// }
+
+// console.log(isOld);
