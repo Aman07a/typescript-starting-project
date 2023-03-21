150d362c20384e4a06f27bb6813786ed7f43dde0
Aman Ahmed <aman16.aa17@gmail.com>
2023-03-21 13:59:48 +0100

    Arrays Types

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+// const person: { name: string; age: number } = {
 var person = {
-    // const person = {}
     name: "Aman",
     age: 22,
+    hobbies: ["Coding", "Gym"],
 };
+var favoriteActivities;
+favoriteActivities = ["Gym"];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); // !!! ERROR !!!
+}
--- app.ts
+++ app.ts
@@ -1,7 +1,16 @@
-const person: { name: string; age: number } = {
-  // const person = {}
+// const person: { name: string; age: number } = {
+const person = {
   name: "Aman",
   age: 22,
+  hobbies: ["Coding", "Gym"],
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ["Gym"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map()); // !!! ERROR !!!
+}
