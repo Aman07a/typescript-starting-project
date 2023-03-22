2c2685d14fb2afd52662b5c8fdd91d45674e0e7c
Aman Ahmed <aman16.aa17@gmail.com>
2023-03-22 12:39:49 +0100

    Shorthand Initialization

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,29 @@
 class Department {
-  public name: string;
+  // private id: string;
+  // private name: string;
   private employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
   }
 
   describe(this: Department) {
-    console.log("Department: " + this.name);
+    console.log(`Department (${this.id}): ${this.name}`);
   }
 
   addEmployee(employee: string) {
+    // validation etc
     this.employees.push(employee);
   }
 
   printEmployeeInformation() {
-    console.log(this.employees);
     console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const accounting = new Department("Accounting");
+const accounting = new Department("d1", "Accounting");
 
 accounting.addEmployee("Aman");
 accounting.addEmployee("Manu");
