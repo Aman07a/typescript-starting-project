d295388fbb837be51ace94ec1468b2a90f08d2c6
Aman Ahmed <aman16.aa17@gmail.com>
2023-03-22 20:37:34 +0100

    Generic Classes

--- src/app.ts
+++ src/app.ts
@@ -42,3 +42,38 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 extractAndConvert({ name: "Aman" }, "name");
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1); // -1
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Aman");
+textStorage.addItem("Manu");
+textStorage.removeItem("Aman");
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+// const objStorage = new DataStorage<object>();
+// const AmanObj = { name: "Aman" };
+// objStorage.addItem(AmanObj);
+// objStorage.addItem({ name: "Manu" });
+// // ...
+// objStorage.removeItem(AmanObj);
+// console.log(objStorage.getItems());
