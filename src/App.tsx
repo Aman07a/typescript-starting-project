6d78e5afdc5c94527cf8c23869fc59def771ef28
Aman Ahmed <aman16.aa17@gmail.com>
2023-03-25 19:11:06 +0100

    Working with Props and Types for Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-function App() {
-  return <div className="App">App</div>;
-}
+import TodoList from "./components/TodoList";
+
+const App: React.FC = () => {
+  const todos = [{ id: "t1", text: "Finish the course" }];
+  return (
+    <div className="App">
+      {/* A component that adds todos */}
+      <TodoList items={todos} />
+    </div>
+  );
+};
 
 export default App;
