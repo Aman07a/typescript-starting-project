f00b9f95a91aa4df0a1d3eee8981ba1787102e83
Aman Ahmed <aman16.aa17@gmail.com>
2023-03-25 15:45:09 +0100

    TypeScript-embracing: class-validato

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import { plainToInstance } from "class-transformer";
+import { validate } from "class-validator";
 
 import { Product } from "./product.module";
 
@@ -8,7 +9,16 @@ const products = [
   { title: "A Book", price: 10.99 },
 ];
 
-// const p1 = new Product('A Book', 12.99);
+const newProd = new Product("", -5.99);
+validate(newProd).then((errors) => {
+  if (errors.length > 0) {
+    console.log("VALIDATION ERRORS");
+    console.log(errors);
+  } else {
+    console.log(newProd.getInformation());
+  }
+});
+console.log(newProd.getInformation());
 
 // const loadedProducts = products.map(prod => {
 //   return new Product(prod.title, prod.price);
